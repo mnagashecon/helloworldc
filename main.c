9b5641c5954e3957085b1b189fd09d0816e1bbf9
mnagashecon <mnagashecon@gmail.com>
2020-09-27 18:41:58 +0900

    [Add] main.c main.exe

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+int func(int i) {
+    printf("i = [%d]\n",i);
+    return(0);
+}
+
 int main(int argc, char** argv) {
+    int i;
+
     printf("Hello World in C\n");
+
+    i = 9;
+    func(i);
+    
     return(0);
 }
